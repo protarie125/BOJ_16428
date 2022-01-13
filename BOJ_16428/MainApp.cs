4d958f7bda60c6ee7cca40acfb3d4e3bd1ff2080
protarie125 <proarie@gmail.com>
2022-01-13 18:56:08 +0900

    초기 커밋.

---
+++ BOJ_16428/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace BOJ_16428
+{
+    internal static class MainApp
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            var ab = line.Split(' ');
+            var a = BigInteger.Parse(ab[0]);
+            var b = BigInteger.Parse(ab[1]);
+
+            var q = a / b;
+            var r = a % b;
+            if (r < 0)
+            {
+                if (0 < b)
+                {
+                    r += b;
+                    q -= 1;
+                }
+                else
+                {
+                    r -= b;
+                    q += 1;
+                }
+            }
+
+            Console.WriteLine(q);
+            Console.WriteLine(r);
+        }
+    }
+}
